101d231cfd2e63111350450da519caef0313b84b
Axel Jacobsen <axelnj44@gmail.com>
2022-08-15 18:06:05 -0700

    working on kmeans

--- cluster_anchors.py
+++ cluster_anchors.py
@@ -3,34 +3,68 @@
 """ K-means clustering of anchors
 """
 
+import glob
+import numpy as np
+import numpy.typing as npt
+
 from collections import namedtuple
 
 
-Box = namedtuple("Box", ["xmin", "ymin", "xmax", "ymax"])
+# [xmin, xmax, ymin, ymax]
+Box = npt.NDArray[np.float64]
+
+
+def xc_yc_w_h_to_corners(xc, yc, w, h):
+    return (
+        xc - w / 2,
+        xc + w / 2,
+        yc - h / 2,
+        yc + h / 2,
+    )
+
+
+def corners_to_xc_yc_w_h(xmin, xmax, ymin, ymax):
+    return (
+        (xmax + xmin) / 2,
+        (ymax + ymin) / 2,
+        (xmax - xmin),
+        (ymax - ymin),
+    )
 
 
 def area(b1: Box):
-    return abs(b1.xmax - b1.xmin) * abs(b1.ymax - b1.ymin)
+    return abs(b1[1] - b1[0]) * abs(b1[3] - b1[2])
 
 
 def iou(b1: Box, b2: Box):
-    inner_xmin = max(b1.xmin, b2.xmin)
-    inner_xmax = min(b1.xmax, b2.xmax)
-    inner_ymin = max(b1.ymin, b2.ymin)
-    inner_ymax = min(b1.ymax, b2.ymax)
+    inner_xmin = max(b1[0], b2[0])
+    inner_xmax = min(b1[1], b2[1])
+    inner_ymin = max(b1[2], b2[2])
+    inner_ymax = min(b1[3], b2[3])
 
     if inner_xmax < inner_xmin or inner_ymax < inner_ymin:
         return 0
 
-    intersection = area(Box(inner_xmin, inner_ymin, inner_xmax, inner_ymax))
+    intersection = area(np.array([inner_xmin, inner_xmax, inner_ymin, inner_ymax]))
     union = area(b1) + area(b2) - intersection
     return intersection / union
 
 
+def get_all_bounding_boxes(bb_dir):
+    bbs = []
+    for fname in glob.glob(f"{bb_dir}/*.csv"):
+        with open(fname, "r") as f:
+            for line in f:
+                vs = [float(v) for v in line.split(",")]
+                bbs.append(xc_yc_w_h_to_corners(*vs[1:]))
+    return np.array(bbs)
 
 
 
 if __name__ == "__main__":
-    b1 = Box(xmin=0, ymin=0, xmax=1, ymax=1)
+    import sys
+
+    b1 = np.array([0, 1, 0, 1])
     print(area(b1))
     print(iou(b1, b1))
+    print(get_all_bounding_boxes(sys.argv[1]).shape)
--- tests.py
+++ tests.py
@@ -1,28 +1,39 @@
 #! /usr/bin/env python3
 
 import unittest
+import numpy as np
 
-from cluster_anchors import Box, area, iou
+from cluster_anchors import Box, area, iou, xc_yc_w_h_to_corners, corners_to_xc_yc_w_h
 
 
 class TestClustering(unittest.TestCase):
     """
     it is best to draw out the bounding boxes for these tests
     """
+
     def test_IOU_sanity_checks(self):
-        b1 = Box(xmin=0, ymin=0, xmax=1, ymax=1)
-        b2 = Box(xmin=1, ymin=1, xmax=2, ymax=2)
-        b3 = Box(xmin=10, ymin=10, xmax=11, ymax=11)
+        b1 = np.array([0, 1, 0, 1])
+        b2 = np.array([1, 2, 1, 2])
+        b3 = np.array([10, 11, 10, 11])
         self.assertEqual(iou(b1, b1), 1.0)
         self.assertEqual(iou(b2, b2), 1.0)
         self.assertEqual(iou(b1, b2), 0)
         self.assertEqual(iou(b1, b3), 0)
 
     def test_IOU_basic(self):
-        b1 = Box(xmin=0, ymin=2, xmax=4, ymax=6)
-        b2 = Box(xmin=2, ymin=0, xmax=6, ymax=4)
-        self.assertEqual(iou(b1, b2), 4 / (4*4 + 4*4 - 4))
-        self.assertEqual(iou(b2, b1), 4 / (4*4 + 4*4 - 4))
+        b1 = np.array([0, 4, 2, 6])
+        b2 = np.array([2, 6, 0, 4])
+        self.assertEqual(iou(b1, b2), 4 / (4 * 4 + 4 * 4 - 4))
+        self.assertEqual(iou(b2, b1), 4 / (4 * 4 + 4 * 4 - 4))
+
+    def test_box_definition_conversions(self):
+        for _ in range(100):
+            corners = np.random.rand(4)
+            self.assertTrue(
+                np.allclose(
+                    corners, xc_yc_w_h_to_corners(*corners_to_xc_yc_w_h(*corners))
+                )
+            )
 
 
 if __name__ == "__main__":
