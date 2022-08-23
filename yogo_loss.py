1bff8304b11ac501ab023e6073df45e5fbcb9c42
Axel Jacobsen <axelnj44@gmail.com>
2022-08-23 16:10:51 -0700

    Basic and slow impl of loss function

--- yogo_loss.py
+++ yogo_loss.py
@@ -5,8 +5,10 @@ Implement YOLO loss function here
 import torch
 import bisect
 
+import torch.nn.functional as F
+
 from operator import itemgetter
-from typing import List, Any, Tuple, Callable
+from typing import Any, List, Dict, Tuple, Callable
 
 """
 Original YOLO paper did not mention IOU?
@@ -29,13 +31,16 @@ class YOGOLoss(torch.nn.modules.loss._Loss):
         self.coord_weight = coord_weight
         self.no_obj_weight = no_obj_weight
         self.mse = torch.nn.MSELoss()
+        self.cel = torch.nn.CrossEntropyLoss()
 
-    def forward(self, pred: torch.Tensor, labels: List[List[float]]) -> torch.Tensor:
+    def forward(
+        self, pred_batch: torch.Tensor, label_batch: List[List[List[float]]]
+    ) -> torch.Tensor:
         """
-        pred and true has shape
+        pred and label are both 4d. pred_batch has shape
         (
              batch size,
-             pred_dim (5 + #classes), (tx, ty, tw, th, to, c1, c2, c3, c4)
+             pred_dim, (tx, ty, tw, th, to, c1, c2, c3, c4)
              Sx,
              Sy
         )
@@ -43,20 +48,48 @@ class YOGOLoss(torch.nn.modules.loss._Loss):
         TODO:
             - "bag of freebies" for free +5% mAP https://arxiv.org/pdf/1902.04103.pdf
         """
-        # TODO - need the lables to be formatted correctly! See following link for details.
+        # TODO - this will be halariously slow! must speed up shortly
+        # TODO - need the lables to be formatted correctly! See following link for loss impl.
         # https://jonathan-hui.medium.com/real-time-object-detection-with-yolo-yolov2-28b1b93e2088
         # IOU may need to be rewritten - used in `t_o` preds
-        batch_size, preds_size, Sx, Sy = pred.shape
-        assert batch_size == len(labels)
+        batch_size, preds_size, Sx, Sy = pred_batch.shape
+        assert batch_size == len(label_batch)
 
-        # return self.coord_weight * (
-        # return self.coord_weight * (
-        #     self.mse(pred[:, :2, :, :], true[:, :2, :, :])
-        #     + self.mse(torch.sqrt(pred[:, :2, :, :]), torch.sqrt(true[:, :2, :, :]))
-        # )
+        loss = torch.tensor(0, dtype=torch.float32)
+        for i in range(batch_size):
+            bins = _split_labels_into_bins(label_batch[i], Sx, Sy)
+
+            for (j, k), Ls in bins.items():
+                if len(Ls) == 0:
+                    loss += self.no_obj_weight * self.mse(
+                        pred_batch[i, 4, j, k], torch.tensor(0)
+                    )
+                elif len(Ls) == 1:
+                    [cls, xc, yc, w, h] = Ls.pop()
+                    loss += self.coord_weight * (
+                        self.mse(pred_batch[i, 0, j, k], torch.tensor(xc))
+                        + self.mse(pred_batch[i, 1, j, k], torch.tensor(yc))
+                        + self.mse(
+                            torch.sqrt(pred_batch[i, 2, j, k]),
+                            torch.sqrt(torch.tensor(w)),
+                        )
+                        + self.mse(
+                            torch.sqrt(pred_batch[i, 3, j, k]),
+                            torch.sqrt(torch.tensor(h)),
+                        )
+                    )
+                    loss += self.mse(pred_batch[i, 4, j, k], torch.tensor(1))
+                    loss += self.cel(
+                        pred_batch[i, 5:, j, k],
+                        F.one_hot(torch.tensor(int(cls)), num_classes=4).float(),
+                    )
+                else:
+                    # TODO: choose max IOU, and pick again
+                    pass
+        return loss
 
 
-def _split_labels_into_bins(labels, Sx, Sy):
+def _split_labels_into_bins(labels, Sx, Sy) -> Dict[Tuple[int, int], List[List[float]]]:
     bins: Dict[Tuple[int, int], List[List[float]]] = dict()
     labels_x_sorted = sorted(labels, key=itemgetter(1))
     x_bins = _get_bin_sorted_list_idxs(labels_x_sorted, Sx, key=itemgetter(1))
@@ -84,43 +117,50 @@ def _get_bin_sorted_list_idxs(
 
 
 if __name__ == "__main__":
-    import matplotlib.pyplot as plt
-    from matplotlib.patches import Rectangle
     from itertools import cycle
-    from cluster_anchors import plot_boxes
     from model import YOGO
     from dataloader import get_dataloader
+
+    def plot(data, l):
+        import matplotlib.pyplot as plt
+        from matplotlib.patches import Rectangle
+
+        "quick and hacky way to debug some split label jazz"
+        LS = _split_labels_into_bins(l, 4, 4)
+        _, ax = plt.subplots()
+        ax.imshow(data[0, 0, :, :])
+
+        _, _, img_h, img_w = data.shape
+        colors = cycle(["r", "g", "b", "c", "m", "y", "k"])
+        # AAAHHHH! AHHHHHHH!!!!
+        for k, ls in LS.items():
+            current_axis = plt.gca()
+            color = next(colors)
+            for i, box in enumerate(ls):
+                [_, xc, yc, w, h] = box
+                current_axis.add_patch(
+                    Rectangle(
+                        (img_w * (xc - w / 2), img_h * (yc - h / 2)),
+                        img_w * w,
+                        img_h * h,
+                        facecolor="none",
+                        edgecolor=color,
+                    )
+                )
+        plt.show()
 
     loss = YOGOLoss()
 
     m = YOGO(anchor_w=36, anchor_h=36)
 
     ODL = get_dataloader("healthy_cell_dataset.yml", batch_size=1)
+
     ii = 5
     for data, labels in ODL["test"]:
         for l in labels:
-            LS = _split_labels_into_bins(l, 4, 4)
-            _, ax = plt.subplots()
-            ax.imshow(data[0, 0, :, :])
+            print(loss(m(data), labels))
+            plot(data, l)
 
-            _, _, img_h, img_w = data.shape
-            colors = cycle(["r", "g", "b", "c", "m", "y", "k"])
-            # AAAHHHH! AHHHHHHH!!!!
-            for k, ls in LS.items():
-                current_axis = plt.gca()
-                color = next(colors)
-                for i, box in enumerate(ls):
-                    [_, xc, yc, w, h] = box
-                    current_axis.add_patch(
-                        Rectangle(
-                            (img_w * (xc - w / 2), img_h * (yc - h / 2)),
-                            img_w * w,
-                            img_h * h,
-                            facecolor="none",
-                            edgecolor=color,
-                        )
-                    )
-            plt.show()
         ii -= 1
         if ii < 0:
-            raise RuntimeError("im freaking out")
+            break
