fix class weighting

model.py
@@ -22,9 +22,9 @@ class YOGO(nn.Module):
     onto the end of forward if we are running inference.
     """
 
-    def __init__(self, img_size, anchor_w, anchor_h, inference=False):
+    def __init__(self, img_size, anchor_w, anchor_h, inference=False, device="cpu"):
         super().__init__()
-        self.device = "cpu"
+        self.device = device
 
         self.backbone = self.gen_backbone()
         self.head = self.gen_head(num_channels=128, num_classes=4)

yogo_loss.py
@@ -26,6 +26,7 @@ class YOGOLoss(torch.nn.modules.loss._Loss):
         coord_weight: float = 5.0,
         no_obj_weight: float = 0.5,
         class_weights: Optional[List[float]] = None,
+        device: str = "cpu"
     ) -> None:
         super().__init__()
         self.coord_weight = coord_weight
@@ -34,7 +35,7 @@ class YOGOLoss(torch.nn.modules.loss._Loss):
         self.cel = torch.nn.CrossEntropyLoss(
             weight=torch.tensor(class_weights), reduction="none", label_smoothing=0.01
         )
-        self.device = "cpu"
+        self.device = device
 
     def to(self, device):
         self.device = device