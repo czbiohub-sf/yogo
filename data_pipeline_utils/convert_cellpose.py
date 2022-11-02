ce81319df74cd711f0f6e11250a18404d252fa9d
Axel Jacobsen <axelnj44@gmail.com>
2022-11-02 11:25:45 -0700

    multiprocess conversion from cellpose to labels so we can get more than 1 thing done in a day

--- data_pipeline_utils/convert_cellpose.py
+++ data_pipeline_utils/convert_cellpose.py
@@ -1,13 +1,15 @@
 #! /usr/bin/env python3
 
 import sys
+import time
 import glob
 
 import numpy as np
-
-from pathlib import Path
+import multiprocessing as mp
 
 from tqdm import tqdm
+from pathlib import Path
+from functools import partial
 from cellpose import utils, io
 
 from _utils import normalize, convert_coords
@@ -16,21 +18,37 @@ from _utils import normalize, convert_coords
 def process_cellpose_results(input_dir, output_dir, label=0):
     output_dir_path = Path(output_dir)
     output_dir_path.mkdir(parents=True, exist_ok=True)
+    work_fnc = partial(work, label, output_dir_path)
 
-    for f in tqdm(glob.glob(f"{input_dir}/*.npy")):
-        outlines = load_cellpose_npy_file(f)
-        file_name = Path(f).name.replace("_seg", "")
-        new_csv = (output_dir_path / file_name).with_suffix(".csv")
-        with open(new_csv, "w") as g:
-            for outline in outlines:
-                xmin, xmax, ymin, ymax = (
-                    outline[:, 0].min(),
-                    outline[:, 0].max(),
-                    outline[:, 1].min(),
-                    outline[:, 1].max(),
-                )
-                xcenter, ycenter, width, height = convert_coords(xmin, xmax, ymin, ymax)
-                g.write(f"{label},{xcenter},{ycenter},{width},{height}\n")
+    files = glob.glob(f"{input_dir}/*.npy")
+    cpu_count = mp.cpu_count()
+
+    print(f"processing {len(files)} files")
+    print(f"num cpus: {cpu_count}")
+
+    with mp.Pool(cpu_count) as P:
+        # list so we get tqdm output, thats it!
+        for _ in tqdm(P.imap_unordered(work_fnc, files, chunksize=64), total=len(files)):
+            pass
+        P.close()
+        P.join()
+
+
+def work(label, output_dir_path, f):
+    outlines = load_cellpose_npy_file(f)
+    file_name = Path(f).name.replace("_seg", "")
+    new_csv = (output_dir_path / file_name).with_suffix(".csv")
+
+    with open(new_csv, "w") as g:
+        for outline in outlines:
+            xmin, xmax, ymin, ymax = (
+                outline[:, 0].min(),
+                outline[:, 0].max(),
+                outline[:, 1].min(),
+                outline[:, 1].max(),
+            )
+            xcenter, ycenter, width, height = convert_coords(xmin, xmax, ymin, ymax)
+            g.write(f"{label},{xcenter},{ycenter},{width},{height}\n")
 
 
 def load_cellpose_npy_file(f):
@@ -51,4 +69,5 @@ if __name__ == "__main__":
     process_cellpose_results(
         sys.argv[1],
         sys.argv[2],
+        label=label
     )
