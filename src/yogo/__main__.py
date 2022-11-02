1f3474659baf7ec84d21c9527ad39033a90e3668
Axel Jacobsen <axelnj44@gmail.com>
2022-11-02 14:41:21 -0700

    optional install onnx because it is fragile :O

--- src/yogo/__main__.py
+++ src/yogo/__main__.py
@@ -1,16 +1,28 @@
+import sys
+
 from yogo.train import do_training
-from yogo.export_model import do_export
 from yogo.argparsers import global_parser
+
+no_onnx = False
+try:
+    from yogo.export_model import do_export
+except ImportError:
+    no_onnx = True
 
 
 def main():
     p = global_parser()
     args = p.parse_args()
 
-    if args.task == "export":
-        do_export(args)
-    elif args.task == "train":
+    if args.task == "train":
         do_training(args)
+    elif args.task == "export":
+        if no_onnx:
+            print("onnx is not installed; install yogo with `pip3 install yogo[onnx]`")
+            sys.exit(1)
+        do_export(args)
+    else:
+        p.print_help()
 
 
 if __name__ == "__main__":
