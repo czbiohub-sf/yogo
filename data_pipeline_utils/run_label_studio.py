da7597e25ab5f0e8204e07b0b3dd15e5fb831888
Axel Jacobsen <axelnj44@gmail.com>
2023-02-15 14:38:27 -0800

    tweaks

--- data_pipeline_utils/run_label_studio.py
+++ data_pipeline_utils/run_label_studio.py
@@ -3,11 +3,38 @@
 
 import os
 import sys
+import argparse
 import subprocess
 
 from pathlib import Path
 from multiprocessing import Process
 from http.server import ThreadingHTTPServer, SimpleHTTPRequestHandler
+
+from label_studio_sdk import Client
+
+from generate_labelstudio_tasks import generate_tasks_for_runset
+
+
+try:
+    boolean_action = argparse.BooleanOptionalAction  # type: ignore
+except AttributeError:
+    boolean_action = "store_true"  # type: ignore
+
+
+def get_parser():
+    """
+    I wanted to add the option to import `tasks.json`, but you need an api key for
+    that. Can we create a 'global' api key that can create projects? dunno!
+    """
+    parser = argparse.ArgumentParser(description="label studio runner!")
+
+    parser.add_argument(
+        "run-folder",
+        type=Path,
+        help="path to run folder (i.e. folder containing 'images' and 'labels'",
+    )
+    return parser
+
 
 
 def run_server(directory: Path):
@@ -20,7 +47,7 @@ def run_server(directory: Path):
 
     httpd = ThreadingHTTPServer(server_addy, Handler)
 
-    print(f"serving your files, Hot n' Fresh, from http://localhost:{port}")
+    print(f"serving your files, Hot n' Fresh, on http://localhost:{port} from {str(directory)}")
     httpd.serve_forever()
 
 
@@ -31,19 +58,22 @@ def run_server_in_proc(directory: Path) -> Process:
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print(f"usage: {sys.argv[0]} <path to runset folder>")
-        sys.exit(1)
+    parser = get_parser()
+    args = parser.parse_args()
 
-    path_to_runset_folder = Path(sys.argv[1])
+    path_to_run_folder = args.run_folder
 
-    if not path_to_runset_folder.exists():
-        raise ValueError(f"{str(path_to_runset_folder)} doesn't exist")
+    if not path_to_run_folder.exists():
+        raise ValueError(f"{str(path_to_run_folder)} doesn't exist")
+    elif (not (path_to_run_folder / "images").exists()) or (not (path_to_run_folder / "labels").exists()):
+        raise FileNotFoundError(f"'images' or 'labels' folder missing in {path_to_run_folder}")
+    elif not (path_to_run_folder / "tasks.json").exists():
+        generate_tasks_for_runset(path_to_run_folder)
 
     os.environ["LABEL_STUDIO_LOCAL_FILES_SERVING_ENABLED"] = "true"
-    os.environ["LABEL_STUDIO_LOCAL_FILES_DOCUMENT_ROOT"] = str(path_to_runset_folder)
+    os.environ["LABEL_STUDIO_LOCAL_FILES_DOCUMENT_ROOT"] = str(path_to_run_folder)
 
-    proc = run_server_in_proc(path_to_runset_folder)
+    proc = run_server_in_proc(path_to_run_folder)
 
     try:
         subprocess.run(["label-studio", "start"])
