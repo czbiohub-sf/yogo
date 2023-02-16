a31bac2b1a63873f886795267892b464c9178d97
Axel Jacobsen <axelnj44@gmail.com>
2023-02-15 17:05:12 -0800

    serve

--- data_pipeline_utils/generate_labelstudio_tasks.py
+++ data_pipeline_utils/generate_labelstudio_tasks.py
@@ -1,37 +1,49 @@
 #! /usr/bin/env python3
 
 
+from urllib.request import pathname2url
 from pathlib import Path
 
-from labelling_constants import IMG_WIDTH, IMG_HEIGHT
+from labelling_constants import IMG_WIDTH, IMG_HEIGHT, FLEXO_DATA_DIR
 
 from label_studio_converter.imports.yolo import convert_yolo_to_ls
+
+
+def tqdm(v):
+    return v
+
+
+if __name__ == "__main__":
+    try:
+        from tqdm import tqdm  # type: ignore
+    except ImportError:
+        pass
 
 
 def generate_tasks_for_runset(path_to_runset_folder: Path):
     folders = [Path(p).parent for p in path_to_runset_folder.glob("./**/labels")]
 
-    def tqdm(v):
-        return v
-    if __name__ == "__main__":
-        try:
-            from tqdm import tqdm  # type: ignore
-        except ImportError:
-            print("missing tqdm")
-            pass
-    else:
-        print("not main")
-
     for folder_path in tqdm(folders):
         if not folder_path.is_dir():
+            print(f"warning: {folder_path} is not a directory")
             continue
 
+        elif not folder_path.is_relative_to(FLEXO_DATA_DIR):
+            print(
+                f"warning: {folder_path} is not relative to our data dirs, {FLEXO_DATA_DIR}"
+            )
+            continue
+
+        abbreviated_path = folder_path.relative_to(FLEXO_DATA_DIR)
+        root_url = str(
+            Path("http://localhost:8081") / pathname2url(abbreviated_path) / "images"
+        )
         try:
             convert_yolo_to_ls(
                 input_dir=str(folder_path),
                 out_file=str(folder_path / "tasks.json"),
                 out_type="predictions",
-                image_root_url="http://localhost:8081",
+                image_root_url=root_url,
                 image_ext=".png",
                 image_width=IMG_WIDTH,
                 image_height=IMG_HEIGHT,
