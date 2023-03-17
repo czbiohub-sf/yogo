657e9d2c81c12a195a8c1c84825704cb61b5aa43
Axel Jacobsen <axelnj44@gmail.com>
2023-03-17 11:09:22 -0700

    option to default to label oprediction selection

--- src/yogo/utils.py
+++ src/yogo/utils.py
@@ -44,7 +44,9 @@ class Metrics:
         mAP_preds, mAP_labels = self.format_for_mAP(preds, labels)
         self.mAP.update(mAP_preds, mAP_labels)
 
-        formatted_preds, formatted_labels = self._format_preds_and_labels(preds, labels)
+        formatted_preds, formatted_labels = self._format_preds_and_labels(
+            preds, labels, use_IoU=True
+        )
 
         self.confusion.update(
             formatted_preds[:, 5:].argmax(dim=1), formatted_labels[:, 5:].squeeze()
@@ -67,59 +69,79 @@ class Metrics:
         self,
         batch_preds: torch.Tensor,
         batch_labels: torch.Tensor,
-        objectness_thresh: float = 0,
-        IoU_thresh: float = 0,
+        use_IoU: bool = True,
+        objectness_thresh: float = 0.3,
     ) -> Tuple[torch.Tensor, torch.Tensor]:
         """A very important utility function for filtering predictions on labels
 
         Often, we need to calculate conditional probabilites - e.g. #(correct predictions | objectness > thresh)
         We want to select our predicted bbs and class predictions on IOU, and sometimes on ojbectness, e.t.c
 
-        batch_preds and batch_labels are the batch label and prediction tensors.
-        objectness_thresh is the "objectness" threshold, YOGO's confidence that there is a prediction in the given cell
-        IoU_thresh is the threshold of IoU for prediction and label bbs.
+        batch_preds and batch_labels are the batch label and prediction tensors, hot n' fresh from the model and dataloader!.
+        use_IoU is whether to use IoU instead of naive cell matching. More accurate, but slower.
+        objectness_thresh is the "objectness" threshold, YOGO's confidence that there is a prediction in the given cell. Can
+            only be used with use_IoU == True
 
         Returns (tensor of predictions shape=[N, x y x y t0 *classes], tensor of labels shape=[N, mask x y x y class])
         """
-        if IoU_thresh != 0:
-            # it isn't immediately obvious to me how exactly to do this. Filter out rows of IoU matrix?
-            # what happens if number of predicted_boxes != number of label_boxes?
-            raise NotImplementedError("axel hasn't implemented `IoU_thresh` yet!")
         if not (0 <= objectness_thresh < 1):
             raise ValueError(
                 f"must have 0 <= objectness_thresh < 1; got objectness_thresh={objectness_thresh}"
             )
 
-        bs1, pred_shape, Sy, Sx = batch_preds.shape
-        bs2, label_shape, Sy, Sx = batch_labels.shape
-        assert bs1 == bs2, "sanity check"
+        (
+            bs1,
+            pred_shape,
+            Sy,
+            Sx,
+        ) = batch_preds.shape  # pred_shape is xc yc w h to *classes
+        (
+            bs2,
+            label_shape,
+            Sy,
+            Sx,
+        ) = batch_labels.shape  # label_shape is mask x y x y class
+        assert bs1 == bs2, f"sanity check, pred batch size should equal"
 
         masked_predictions, masked_labels = [], []
         for b in range(bs1):
-            # xc yc w h to *classes
             reformatted_preds = batch_preds[b, ...].view(pred_shape, Sx * Sy).T
-
-            # mask x y x y class
             reformatted_labels = batch_labels[b, ...].view(label_shape, Sx * Sy).T
 
-            # masked labels is *actual predictions*
-            img_masked_labels = reformatted_labels[reformatted_labels[:, 0].bool()]
+            # reformatted_labels[:, 0] = 1 if there is a label for that cell, else 0
+            labels_mask = reformatted_labels[:, 0].bool()
+            objectness_mask = (reformatted_preds[:, 4] > objectness_thresh).bool()
 
-            # filter on objectness
-            preds_with_objects = reformatted_preds[
-                reformatted_preds[:, 4] > objectness_thresh
-            ]
+            img_masked_labels = reformatted_labels[labels_mask]
 
-            preds_with_objects[:, 0:4] = ops.box_convert(
-                preds_with_objects[:, 0:4], "cxcywh", "xyxy"
-            )
+            if use_IoU and objectness_mask.sum() >= len(img_masked_labels):
+                # filter on objectness
+                preds_with_objects = reformatted_preds[objectness_mask]
 
-            # choose predictions from argmaxed IoU along label dim to get best prediction per label
-            prediction_indices = ops.box_iou(
-                img_masked_labels[:, 1:5], preds_with_objects[:, 0:4]
-            ).argmax(dim=1)
+                preds_with_objects[:, 0:4] = ops.box_convert(
+                    preds_with_objects[:, 0:4], "cxcywh", "xyxy"
+                )
 
-            masked_predictions.append(preds_with_objects[prediction_indices])
+                # choose predictions from argmaxed IoU along label dim to get best prediction per label
+                prediction_indices = ops.box_iou(
+                    img_masked_labels[:, 1:5], preds_with_objects[:, 0:4]
+                ).argmax(dim=1)
+                final_preds = preds_with_objects[prediction_indices]
+            else:
+                if use_IoU:
+                    # we know that objectness_mask.sum() < len(img_masked_labels) - i.e. there are
+                    # fewer predicted objects than labels.
+                    print(
+                        "warning (utils._format_preds_and_labels) fewer predicted objects "
+                        "than labels, defaulting to label mask."
+                    )
+                # filter on label tensor idx
+                final_preds = reformatted_preds[reformatted_labels[:, 0].bool()]
+                final_preds[:, 0:4] = ops.box_convert(
+                    final_preds[:, 0:4], "cxcywh", "xyxy"
+                )
+
+            masked_predictions.append(final_preds)
             masked_labels.append(img_masked_labels)
 
         return torch.cat(masked_predictions), torch.cat(masked_labels)
@@ -249,11 +271,7 @@ if __name__ == "__main__":
         sys.exit(1)
 
     path_to_ddf = sys.argv[1]
-    ds = get_dataloader(
-        path_to_ddf,
-        batch_size=1,
-        training=False,
-    )
+    ds = get_dataloader(path_to_ddf, batch_size=1, training=False,)
 
     for img, label in ds["val"]:
         imshow(draw_rects(img[0, 0, ...], list(label[0])))
