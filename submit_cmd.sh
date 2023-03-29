902292e99e9e620bf0c6079f048e1a06ac898711
Axel Jacobsen <axelnj44@gmail.com>
2023-03-29 14:27:13 -0700

    fix

--- submit_cmd.sh
+++ submit_cmd.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
 #SBATCH --partition=gpu
-#SBATCH --gpus-per-node=a40:1
+#SBATCH --gpus-per-node=1
 #SBATCH --cpus-per-task=16
 #SBATCH --output=./slurm-outputs/slurm-%j.out
 
