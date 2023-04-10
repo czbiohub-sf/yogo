c571491195f0a5cc9478d82d4662f705c5d93a5d
Axel Jacobsen <axelnj44@gmail.com>
2023-04-10 13:37:42 -0700

    remove tweaked kernel

--- src/yogo/model_funcs.py
+++ src/yogo/model_funcs.py
@@ -48,55 +48,6 @@ def base_model(num_classes) -> nn.Module:
     )
 
 
-def base_model_tweaked_kernel(num_classes) -> nn.Module:
-    conv_block_1 = nn.Sequential(
-        nn.Conv2d(1, 16, 5, stride=2, padding=1),
-        nn.LeakyReLU(),
-        nn.BatchNorm2d(16),
-    )
-    conv_block_2 = nn.Sequential(
-        nn.Conv2d(16, 32, 3, padding=1),
-        nn.LeakyReLU(),
-        nn.BatchNorm2d(32),
-    )
-    conv_block_3 = nn.Sequential(
-        nn.Conv2d(32, 64, 5, stride=2, padding=1),
-        nn.LeakyReLU(),
-        nn.BatchNorm2d(64),
-    )
-    conv_block_4 = nn.Sequential(
-        nn.Conv2d(64, 128, 3, padding=1),
-        nn.LeakyReLU(),
-        nn.BatchNorm2d(128),
-    )
-    conv_block_5 = nn.Sequential(
-        nn.Conv2d(128, 128, 5, stride=2, padding=1),
-        nn.LeakyReLU(),
-        nn.BatchNorm2d(128),
-    )
-    conv_block_6 = nn.Sequential(
-        nn.Conv2d(128, 128, 3, padding=1),
-        nn.LeakyReLU(),
-        nn.BatchNorm2d(128),
-    )
-    conv_block_7 = nn.Sequential(
-        nn.Conv2d(128, 128, 3, padding=1),
-        nn.LeakyReLU(),
-        nn.BatchNorm2d(128),
-    )
-    conv_block_8 = nn.Conv2d(128, 5 + num_classes, 1)
-    return nn.Sequential(
-        conv_block_1,
-        conv_block_2,
-        conv_block_3,
-        conv_block_4,
-        conv_block_5,
-        conv_block_6,
-        conv_block_7,
-        conv_block_8,
-    )
-
-
 def model_no_dropout(num_classes: int) -> nn.Module:
     conv_block_1 = nn.Sequential(
         nn.Conv2d(1, 16, 3, stride=2, padding=1),
@@ -376,6 +327,7 @@ def get_model_func(
     model_name: Optional[str],
 ) -> Optional[Callable[[int,], nn.Module]]:
     models = {
+        "base_model": base_model,
         "model_no_dropout": model_no_dropout,
         "model_smaller_SxSy": model_smaller_SxSy,
         "model_big_simple": model_big_simple,
