0658e8df357a0941a4332744efe87909d57b334a
Axel Jacobsen <axelnj44@gmail.com>
2023-04-11 09:47:53 -0700

    reduce kernel size for stride 2 convs

--- src/yogo/model_funcs.py
+++ src/yogo/model_funcs.py
@@ -5,7 +5,7 @@ from torch import nn
 
 def base_model(num_classes) -> nn.Module:
     conv_block_1 = nn.Sequential(
-        nn.Conv2d(1, 16, 7, stride=2, padding=1, bias=False),
+        nn.Conv2d(1, 16, 5, stride=2, padding=1, bias=False),
         nn.BatchNorm2d(16),
         nn.LeakyReLU(),
     )
@@ -14,7 +14,7 @@ def base_model(num_classes) -> nn.Module:
         nn.LeakyReLU(),
     )
     conv_block_3 = nn.Sequential(
-        nn.Conv2d(32, 64, 7, stride=2, padding=1),
+        nn.Conv2d(32, 64, 5, stride=2, padding=1),
         nn.BatchNorm2d(64),
         nn.LeakyReLU(),
     )
@@ -23,7 +23,7 @@ def base_model(num_classes) -> nn.Module:
         nn.LeakyReLU(),
     )
     conv_block_5 = nn.Sequential(
-        nn.Conv2d(128, 128, 7, stride=2, padding=1, bias=False),
+        nn.Conv2d(128, 128, 5, stride=2, padding=1, bias=False),
         nn.BatchNorm2d(128),
         nn.LeakyReLU(),
     )
