ffe039f98ab18b97045613a327d6a41b93c85bda
Axel Jacobsen <axelnj44@gmail.com>
2023-05-09 14:35:13 -0700

    add imports in __init__

--- yogo/__init__.py
+++ yogo/__init__.py
@@ -6,3 +6,11 @@ class DefaultHyperparams:
     DECAY_FACTOR = 10
     WEIGHT_DECAY = 1e-2
     OPTIMIZER_TYPE = "adam"
+
+
+from . import infer
+from . import model
+from . import utils
+from . import data
+
+__all__ = ("infer", "model", "utils", "data")
