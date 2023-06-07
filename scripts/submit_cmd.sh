3137def4898836125f20330662c8977eb3e65f8b
Axel Jacobsen <axelnj44@gmail.com>
2023-06-07 09:09:21 -0700

    fm 60% to 90%+ gpu util

--- scripts/submit_cmd.sh
+++ scripts/submit_cmd.sh
@@ -6,7 +6,7 @@
 #SBATCH --nodes=1
 #SBATCH --partition=gpu
 #SBATCH --gpus-per-node=a100:1
-#SBATCH --cpus-per-task=16
+#SBATCH --cpus-per-task=32
 #SBATCH --output=./slurm-outputs/%j.out
 
 env | grep "^SLURM" | sort
