363a99a762b7e62fe367a794de8af9e78ae5397a
Axel Jacobsen <axelnj44@gmail.com>
2023-06-15 13:30:51 -0700

    af tests

--- test/test_count_predictions.py
+++ test/test_count_predictions.py
@@ -5,24 +5,18 @@ from yogo.infer import _count_class_predictions
 
 
 class TestCountClassPredictions(unittest.TestCase):
-
     def test_simple_class_predictions(self):
         inp = torch.zeros(3, 5)
-        inp[:,0] = 1
+        inp[:, 0] = 1
         expected_result = torch.tensor([3, 0, 0, 0, 0], dtype=torch.long)
-        torch.testing.assert_close(
-            _count_class_predictions(inp),
-            expected_result
-        )
+        torch.testing.assert_close(_count_class_predictions(inp), expected_result)
 
     def test_float_predictions(self):
         row = torch.tensor([0.1, 0.2, 0.3, 0.4])
         inp = torch.stack([row, row, row])
         expected_result = torch.tensor([0, 0, 0, 3], dtype=torch.long)
-        torch.testing.assert_close(
-            _count_class_predictions(inp),
-            expected_result
-        )
+        torch.testing.assert_close(_count_class_predictions(inp), expected_result)
+
 
 if __name__ == "__main__":
     unittest.main()
--- test/test_utils_tensor_formatting.py
+++ test/test_utils_tensor_formatting.py
@@ -14,20 +14,20 @@ class TestFormatPreds(unittest.TestCase):
         - 6th-12th elements are class probabilities
         """
         # no predictions
-        self.no_predictions = torch.zeros(12,4,4).float()
+        self.no_predictions = torch.zeros(12, 4, 4).float()
 
         # single prediction with box of 0 width and height
-        self.single_prediction = torch.zeros(12,4,4).float()
+        self.single_prediction = torch.zeros(12, 4, 4).float()
         # setting objectness > thresh (default 0.5) in grid cell 0,0
-        self.single_prediction[4, 0, 0] = 1.
+        self.single_prediction[4, 0, 0] = 1.0
         # one-hot class pred
-        self.single_prediction[5, :, :] = 1.
+        self.single_prediction[5, :, :] = 1.0
 
         # single prediction with box of 0 width and height in xcycwh format
         # put it in grid cell 1,1 so we can combine with other predictions
-        self.single_prediction_with_cxcywh_box = torch.zeros(12,4,4).float()
-        self.single_prediction_with_cxcywh_box[5, :, :] = 1.  # one-hot class pred
-        self.single_prediction_with_cxcywh_box[4, 1, 1] = 1.
+        self.single_prediction_with_cxcywh_box = torch.zeros(12, 4, 4).float()
+        self.single_prediction_with_cxcywh_box[5, :, :] = 1.0  # one-hot class pred
+        self.single_prediction_with_cxcywh_box[4, 1, 1] = 1.0
         # box of width and height 0.1 at center (0.5,0.5) of image
         self.single_prediction_with_cxcywh_box[0, 1, 1] = 0.5
         self.single_prediction_with_cxcywh_box[1, 1, 1] = 0.5
@@ -38,7 +38,9 @@ class TestFormatPreds(unittest.TestCase):
         """
         test that a tensor with no predictions returns an empty tensor of shape 0,12
         """
-        torch.testing.assert_close(format_preds(self.no_predictions), torch.empty(0,12))
+        torch.testing.assert_close(
+            format_preds(self.no_predictions), torch.empty(0, 12)
+        )
 
     def test_single_prediction(self):
         pred = format_preds(self.single_prediction)
@@ -49,15 +51,17 @@ class TestFormatPreds(unittest.TestCase):
         pred = format_preds(self.single_prediction_with_cxcywh_box)
         # make sure we get the prediction that we set, and that the box format is untouched
         # since the default is cxcywh
-        torch.testing.assert_close(pred, self.single_prediction_with_cxcywh_box[:, 1, 1].unsqueeze(0))
+        torch.testing.assert_close(
+            pred, self.single_prediction_with_cxcywh_box[:, 1, 1].unsqueeze(0)
+        )
 
     def test_single_prediction_with_xyxy_box(self):
         pred = format_preds(self.single_prediction_with_cxcywh_box, box_format="xyxy")
         # make sure we get the prediction that we set, and that the box format is untouched
         # since the default is cxcywh
         actual = self.single_prediction_with_cxcywh_box[:, 1, 1].unsqueeze(0)
-        actual[:,0] = actual[:,0] - actual[:,2] / 2
-        actual[:,1] = actual[:,1] - actual[:,3] / 2
-        actual[:,2] = actual[:,0] + actual[:,2]
-        actual[:,3] = actual[:,1] + actual[:,3]
+        actual[:, 0] = actual[:, 0] - actual[:, 2] / 2
+        actual[:, 1] = actual[:, 1] - actual[:, 3] / 2
+        actual[:, 2] = actual[:, 0] + actual[:, 2]
+        actual[:, 3] = actual[:, 1] + actual[:, 3]
         torch.testing.assert_close(pred, actual)
