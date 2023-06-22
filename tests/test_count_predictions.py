c85a842e5cc65b38925903472a21a56a814f3a94
Axel Jacobsen <axelnj44@gmail.com>
2023-06-22 13:32:51 -0700

    af

--- tests/test_count_predictions.py
+++ tests/test_count_predictions.py
@@ -9,13 +9,17 @@ class TestCountClassPredictions(unittest.TestCase):
         inp = torch.zeros(3, 5)
         inp[:, 0] = 1
         expected_result = torch.tensor([3, 0, 0, 0, 0], dtype=torch.long)
-        torch.testing.assert_close(count_cells_for_formatted_preds(inp), expected_result)
+        torch.testing.assert_close(
+            count_cells_for_formatted_preds(inp), expected_result
+        )
 
     def test_float_predictions(self):
         row = torch.tensor([0.1, 0.2, 0.3, 0.4])
         inp = torch.stack([row, row, row])
         expected_result = torch.tensor([0, 0, 0, 3], dtype=torch.long)
-        torch.testing.assert_close(count_cells_for_formatted_preds(inp), expected_result)
+        torch.testing.assert_close(
+            count_cells_for_formatted_preds(inp), expected_result
+        )
 
 
 if __name__ == "__main__":
