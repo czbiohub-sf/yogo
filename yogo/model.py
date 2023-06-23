4e9579258038e963a7ece1bd68beb6c1313ec940
Axel Jacobsen <axelnj44@gmail.com>
2023-06-23 15:09:02 -0700

    no padding! no! no!

--- yogo/model.py
+++ yogo/model.py
@@ -251,38 +251,38 @@ class YOGO(nn.Module):
 
     def gen_model(self, num_classes) -> nn.Module:
         conv_block_1 = nn.Sequential(
-            nn.Conv2d(1, 16, 3, stride=2, padding=1, bias=False),
+            nn.Conv2d(1, 16, 3, stride=2, bias=False),
             nn.BatchNorm2d(16),
             nn.LeakyReLU(),
             nn.Dropout2d(p=0.2),
         )
         conv_block_2 = nn.Sequential(
-            nn.Conv2d(16, 32, 3, padding=1),
+            nn.Conv2d(16, 32, 3),
             nn.LeakyReLU(),
             nn.Dropout2d(p=0.2),
         )
         conv_block_3 = nn.Sequential(
-            nn.Conv2d(32, 64, 3, stride=2, padding=1),
+            nn.Conv2d(32, 64, 3, stride=2),
             nn.LeakyReLU(),
             nn.Dropout2d(p=0.2),
         )
         conv_block_4 = nn.Sequential(
-            nn.Conv2d(64, 128, 3, padding=1),
+            nn.Conv2d(64, 128, 3),
             nn.LeakyReLU(),
             nn.Dropout2d(p=0.2),
         )
         conv_block_5 = nn.Sequential(
-            nn.Conv2d(128, 128, 3, stride=2, padding=1, bias=False),
+            nn.Conv2d(128, 128, 3, stride=2, bias=False),
             nn.BatchNorm2d(128),
             nn.LeakyReLU(),
         )
         conv_block_6 = nn.Sequential(
-            nn.Conv2d(128, 128, 3, padding=1, bias=True),
+            nn.Conv2d(128, 128, 3, bias=True),
             nn.BatchNorm2d(128),
             nn.LeakyReLU(),
         )
         conv_block_7 = nn.Sequential(
-            nn.Conv2d(128, 128, 3, padding=1),
+            nn.Conv2d(128, 128, 3),
             nn.LeakyReLU(),
         )
         conv_block_8 = nn.Conv2d(128, 5 + num_classes, 1)
