07a7d7243c626678e538e1b5f03bb97186f8f79b
Axel Jacobsen <axelnj44@gmail.com>
2023-07-21 16:43:57 -0700

    smaller model less overparametized??

--- yogo/model.py
+++ yogo/model.py
@@ -272,25 +272,21 @@ class YOGO(nn.Module):
             nn.Dropout2d(p=0.2),
         )
         conv_block_4 = nn.Sequential(
-            nn.Conv2d(64, 128, 3, padding=1),
+            nn.Conv2d(64, 64, 3, padding=1),
             nn.LeakyReLU(),
             nn.Dropout2d(p=0.2),
         )
         conv_block_5 = nn.Sequential(
-            nn.Conv2d(128, 128, 3, stride=2, padding=1, bias=False),
-            nn.BatchNorm2d(128),
+            nn.Conv2d(64, 64, 3, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(64),
             nn.LeakyReLU(),
         )
         conv_block_6 = nn.Sequential(
-            nn.Conv2d(128, 128, 3, padding=1, bias=True),
-            nn.BatchNorm2d(128),
+            nn.Conv2d(64, 64, 3, padding=1, bias=True),
+            nn.BatchNorm2d(64),
             nn.LeakyReLU(),
         )
-        conv_block_7 = nn.Sequential(
-            nn.Conv2d(128, 128, 3, padding=1),
-            nn.LeakyReLU(),
-        )
-        conv_block_8 = nn.Conv2d(128, 5 + num_classes, 1)
+        conv_block_8 = nn.Conv2d(64, 5 + num_classes, 1)
         return nn.Sequential(
             conv_block_1,
             conv_block_2,
@@ -298,7 +294,6 @@ class YOGO(nn.Module):
             conv_block_4,
             conv_block_5,
             conv_block_6,
-            conv_block_7,
             conv_block_8,
         )
 
