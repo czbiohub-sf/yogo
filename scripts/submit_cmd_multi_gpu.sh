67ceac029a8804e820795f35fa1e437ad34d0a59
Axel Jacobsen <axelnj44@gmail.com>
2023-08-04 16:33:18 -0700

    multi-gpu sbatch script

---
+++ scripts/submit_cmd_multi_gpu.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+#SBATCH --job-name=YOGOTraining
+#SBATCH --time=24:00:00
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --partition=gpu
+#SBATCH --gpus-per-node=a100:4
+#SBATCH --cpus-per-task=64
+#SBATCH --output=./slurm-outputs/%j.out
+
+env | grep "^SLURM" | sort
+
+nvcc --version
+
+nvidia-smi
+
+echo "running: $@"
+
+wandb enabled
+wandb online
+conda run "$@"
+wandb offline
+wandb disabled
