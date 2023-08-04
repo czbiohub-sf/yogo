a9123770b632c6bcb4ee045db050a138430c80f8
Axel Jacobsen <axelnj44@gmail.com>
2023-08-04 11:30:11 -0700

    DataParallel is quite easy :)

--- scripts/submit_cmd.sh
+++ scripts/submit_cmd.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
 #SBATCH --partition=gpu
-#SBATCH --gpus-per-node=a100:1
+#SBATCH --gpus-per-node=a100:4
 #SBATCH --cpus-per-task=64
 #SBATCH --output=./slurm-outputs/%j.out
 
