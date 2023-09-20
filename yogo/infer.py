0dd91fb5cd89d24369b34b564db22d943bc77632
Axel Jacobsen <axelnj44@gmail.com>
2023-09-19 18:24:52 -0700

    add mps as potential device

--- yogo/infer.py
+++ yogo/infer.py
@@ -31,7 +31,12 @@ def argmax(arr):
 
 
 def choose_device():
-    return torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    if torch.cuda.is_available():
+        return torch.device("cuda")
+    elif torch.backends.mps.is_available():
+        return torch.device("mps")
+    else:
+        return torch.device("cpu")
 
 
 def save_predictions(
