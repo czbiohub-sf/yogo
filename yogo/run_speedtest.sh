da289891be689cc72ef755495afa406efaa1975f
Axel Jacobsen <axelnj44@gmail.com>
2023-10-06 10:59:14 -0700

    speedy!

---
+++ yogo/run_speedtest.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/bash
+#
+# usage: speedtest.py [-h] [--N N] [--BS BS] [--print-header PRINT_HEADER] pth_path
+# speedtest.py: error: the following arguments are required: pth_path
+
+
+rm speedtest_results.csv
+for power in $(seq 0 12); do
+
+  bs=$(echo "2^$power" | bc)
+
+  if [ $bs -eq 1 ]
+  then
+    ./speedtest.py ../trained_models/bumbling-night-1847/best.pth --N 1000 --BS $bs --print-header >> speedtest_results.csv
+  else
+    ./speedtest.py ../trained_models/bumbling-night-1847/best.pth --N 1000 --BS $bs >> speedtest_results.csv
+  fi
+done
---
+++ yogo/speedtest.py
@@ -0,0 +1,45 @@
+#! /usr/bin/env python3
+
+import time
+import torch
+import argparse
+
+from pathlib import Path
+
+from yogo.model import YOGO
+from yogo.utils import choose_device
+
+parser=argparse.ArgumentParser()
+parser.add_argument("pth_path", help="path to pth", type=Path)
+parser.add_argument("--N", help="num inferences", type=int, default=100)
+parser.add_argument("--BS", help="batch size", type=int, default=1)
+parser.add_argument("--print-header", action='store_true')
+args = parser.parse_args()
+
+device=choose_device()
+
+model, cfg = YOGO.from_pth(Path(args.pth_path), inference=True)
+model.resize_model(193)
+model.to(device)
+model.eval()
+
+BS = args.BS
+N = args.N
+
+inp = torch.rand(BS, 1, 193, 1032).to(device)
+
+# warmup
+for _ in range(100):
+    model(inp)
+
+t0 = time.perf_counter()
+
+for _ in range(N):
+    model(inp)
+
+t1 = time.perf_counter()
+
+if args.print_header:
+    print("num_inferences,batch_size,tot_images,tot_time,FPS")
+print(f"{N},{BS},{BS * N},{t1 - t0},{BS * N / (t1 - t0)}")
+exit()
