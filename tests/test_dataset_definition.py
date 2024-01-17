09a582f4e8fc5beb526e520ee6641d6edfbb316a
Axel Jacobsen <axelnj44@gmail.com>
2024-01-16 16:06:43 -0800

    test checking dset pathhs

--- tests/test_dataset_definition.py
+++ tests/test_dataset_definition.py
@@ -16,7 +16,7 @@ TEST_DIR = Path(__file__).parent
 DEFNS_PATH = TEST_DIR / "fake-data" / "defns"
 
 
-def test_basic_load():
+def test_basic_load() -> None:
     """should successfully load each and have one path per"""
     for basic_defn in ("literal_1.yml", "literal_2.yml", "literal_3.yml"):
         dataset_defn = DatasetDefinition.from_yaml(DEFNS_PATH / basic_defn)
@@ -28,7 +28,7 @@ def test_basic_load():
     assert len(dataset_defn.test_dataset_paths) == 0
 
 
-def test_basic_recursive_load_0():
+def test_basic_recursive_load_0() -> None:
     """
     loading a recursive defn that loads one literal defn
     should be equivalent to loading the literal defn
@@ -38,7 +38,7 @@ def test_basic_recursive_load_0():
     assert literal_defn == recursive_defn
 
 
-def test_basic_recursive_load_1():
+def test_basic_recursive_load_1() -> None:
     """
     should be able to load both a recursive and a literal definition in one file
     """
@@ -51,7 +51,7 @@ def test_basic_recursive_load_1():
     assert literal_concat == recursive_defn
 
 
-def test_basic_recursive_load_2():
+def test_basic_recursive_load_2() -> None:
     """
     equality of dataset definitions is agnostic to order of the lists
     """
@@ -60,7 +60,7 @@ def test_basic_recursive_load_2():
     assert recursive_12 == recursive_21
 
 
-def test_basic_recursive_load_3():
+def test_basic_recursive_load_3() -> None:
     """
     should be able to recur more than once
     """
@@ -69,7 +69,7 @@ def test_basic_recursive_load_3():
     assert recursive_123 == literal_123
 
 
-def test_recursive_cycle_0():
+def test_recursive_cycle_0() -> None:
     """
     should detect a cycle
     """
@@ -85,7 +85,7 @@ def test_recursive_cycle_0():
         DatasetDefinition.from_yaml(DEFNS_PATH / "cycle_3.yml")
 
 
-def test_recursive_cycle_1():
+def test_recursive_cycle_1() -> None:
     """
     should detect a cycle
     """
@@ -93,9 +93,17 @@ def test_recursive_cycle_1():
         DatasetDefinition.from_yaml(DEFNS_PATH / "cycle_self.yml")
 
 
-def test_unique_paths():
+def test_unique_paths() -> None:
     """
     should detect duplicate paths
     """
     with pytest.raises(InvalidDatasetDefinitionFile):
         DatasetDefinition.from_yaml(DEFNS_PATH / "duplicate_paths.yml")
+
+
+def test_path_check() -> None:
+    """
+    should detect missing paths
+    """
+    with pytest.raises(FileNotFoundError):
+        DatasetDefinition.from_yaml(DEFNS_PATH / "literal-non-existant.yml")
