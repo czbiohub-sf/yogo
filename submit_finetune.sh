1948ad3cd2d1ab315e2afa088aa429e6e29dd1e3
Axel Jacobsen <axelnj44@gmail.com>
2024-02-23 13:28:58 -0800

    finetune script... :o :) :/

---
+++ submit_finetune.sh
@@ -0,0 +1,76 @@
+#! /usr/bin/env bash
+
+sbatch scripts/submit_cmd_multi_gpu_light.sh yogo train \
+   --from-pretrained trained_models/vibrant-rooster-557/best.pth \
+   --lr 5e-5 \
+   --batch-size 64 \
+   --epochs 128 \
+   --label-smoothing 5e-3 \
+   --weight-decay 5e-3 \
+   --no-obj-weight 1 \
+   ../dataset_defs/human-labels/all-dataset-subsets.yml \
+   --note "quarter_filters" \
+   --lr-decay-factor 16 \
+   --model quarter_filters \
+   --tag scaling-law \
+   --normalize-images
+
+sbatch scripts/submit_cmd_multi_gpu_light.sh yogo train \
+   --from-pretrained BLAAAANK \
+   --lr 5e-5 \
+   --batch-size 64 \
+   --epochs 128 \
+   --label-smoothing 5e-3 \
+   --weight-decay 5e-3 \
+   --no-obj-weight 1 \
+   ../dataset_defs/human-labels/all-dataset-subsets.yml \
+   --note "half_filters" \
+   --lr-decay-factor 16 \
+   --model half_filters \
+   --tag scaling-law \
+   --normalize-images
+
+sbatch scripts/submit_cmd_multi_gpu_light.sh yogo train \
+   --from-pretrained trained_models/fortuitous-orchid-553/best.pth \
+   --lr 5e-5 \
+   --batch-size 64 \
+   --epochs 128 \
+   --label-smoothing 5e-3 \
+   --weight-decay 5e-3 \
+   --no-obj-weight 1 \
+   ../dataset_defs/human-labels/all-dataset-subsets.yml \
+   --note "base" \
+   --lr-decay-factor 16 \
+   --model base_model \
+   --tag scaling-law \
+   --normalize-images
+
+sbatch scripts/submit_cmd_multi_gpu_light.sh yogo train \
+   --from-pretrained trained_models/sparkling-paper-555/best.pth \
+   --lr 5e-5 \
+   --batch-size 64 \
+   --epochs 128 \
+   --label-smoothing 5e-3 \
+   --weight-decay 5e-3 \
+   --no-obj-weight 1 \
+   ../dataset_defs/human-labels/all-dataset-subsets.yml \
+   --note "double_filters" \
+   --lr-decay-factor 16 \
+   --model double_filters \
+   --tag scaling-law \
+   --normalize-images
+
+sbatch scripts/submit_cmd_multi_gpu_light.sh yogo train \
+   --from-pretrained trained_models/crimson-mandu-556/best.pth \
+   --lr 5e-5 \
+   --batch-size 64 \
+   --epochs 128 \
+   --label-smoothing 5e-3 \
+   --weight-decay 5e-3 \
+   --no-obj-weight 1 \
+   ../dataset_defs/human-labels/all-dataset-subsets.yml \
+   --note "triple_filters" \3
+   --lr-decay-factor 16 \
+   --model triple_filters \
+   --tag scaling-law \
+   --normalize-images
