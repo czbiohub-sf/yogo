565ec20b5deffc9d56df1453244d81a1653db64b
Axel Jacobsen <axelnj44@gmail.com>
2024-02-23 13:31:11 -0800

    >:^)

--- submit_finetune.sh
+++ submit_finetune.sh
@@ -1,14 +1,14 @@
 #! /usr/bin/env bash
 
 sbatch scripts/submit_cmd_multi_gpu_light.sh yogo train \
-   --from-pretrained trained_models/vibrant-rooster-557/best.pth \
+   ../../dataset_defs/human-labels/all-dataset-subsets.yml \
+   --from-pretrained ../trained_models/vibrant-rooster-557/best.pth \
    --lr 5e-5 \
    --batch-size 64 \
    --epochs 128 \
    --label-smoothing 5e-3 \
    --weight-decay 5e-3 \
    --no-obj-weight 1 \
-   ../dataset_defs/human-labels/all-dataset-subsets.yml \
    --note "quarter_filters" \
    --lr-decay-factor 16 \
    --model quarter_filters \
@@ -16,6 +16,7 @@ sbatch scripts/submit_cmd_multi_gpu_light.sh yogo train \
    --normalize-images
 
 sbatch scripts/submit_cmd_multi_gpu_light.sh yogo train \
+   ../../dataset_defs/human-labels/all-dataset-subsets.yml \
    --from-pretrained BLAAAANK \
    --lr 5e-5 \
    --batch-size 64 \
@@ -23,7 +24,6 @@ sbatch scripts/submit_cmd_multi_gpu_light.sh yogo train \
    --label-smoothing 5e-3 \
    --weight-decay 5e-3 \
    --no-obj-weight 1 \
-   ../dataset_defs/human-labels/all-dataset-subsets.yml \
    --note "half_filters" \
    --lr-decay-factor 16 \
    --model half_filters \
@@ -31,14 +31,14 @@ sbatch scripts/submit_cmd_multi_gpu_light.sh yogo train \
    --normalize-images
 
 sbatch scripts/submit_cmd_multi_gpu_light.sh yogo train \
-   --from-pretrained trained_models/fortuitous-orchid-553/best.pth \
+   ../../dataset_defs/human-labels/all-dataset-subsets.yml \
+   --from-pretrained ../trained_models/fortuitous-orchid-553/best.pth \
    --lr 5e-5 \
    --batch-size 64 \
    --epochs 128 \
    --label-smoothing 5e-3 \
    --weight-decay 5e-3 \
    --no-obj-weight 1 \
-   ../dataset_defs/human-labels/all-dataset-subsets.yml \
    --note "base" \
    --lr-decay-factor 16 \
    --model base_model \
@@ -46,14 +46,14 @@ sbatch scripts/submit_cmd_multi_gpu_light.sh yogo train \
    --normalize-images
 
 sbatch scripts/submit_cmd_multi_gpu_light.sh yogo train \
-   --from-pretrained trained_models/sparkling-paper-555/best.pth \
+   ../../dataset_defs/human-labels/all-dataset-subsets.yml \
+   --from-pretrained ../trained_models/sparkling-paper-555/best.pth \
    --lr 5e-5 \
    --batch-size 64 \
    --epochs 128 \
    --label-smoothing 5e-3 \
    --weight-decay 5e-3 \
    --no-obj-weight 1 \
-   ../dataset_defs/human-labels/all-dataset-subsets.yml \
    --note "double_filters" \
    --lr-decay-factor 16 \
    --model double_filters \
@@ -61,14 +61,14 @@ sbatch scripts/submit_cmd_multi_gpu_light.sh yogo train \
    --normalize-images
 
 sbatch scripts/submit_cmd_multi_gpu_light.sh yogo train \
-   --from-pretrained trained_models/crimson-mandu-556/best.pth \
+   ../../dataset_defs/human-labels/all-dataset-subsets.yml \
+   --from-pretrained ../trained_models/crimson-mandu-556/best.pth \
    --lr 5e-5 \
    --batch-size 64 \
    --epochs 128 \
    --label-smoothing 5e-3 \
    --weight-decay 5e-3 \
    --no-obj-weight 1 \
-   ../dataset_defs/human-labels/all-dataset-subsets.yml \
    --note "triple_filters" \3
    --lr-decay-factor 16 \
    --model triple_filters \
--- submit_pretrain.sh
+++ submit_pretrain.sh
@@ -1,13 +1,13 @@
 #! /usr/bin/env bash
 
 sbatch scripts/submit_cmd_multi_gpu_light.sh yogo train \
+   ../dataset_defs/pre-training/yogo_parasite_data_with_tests.yml \
    --lr 5e-4 \
    --batch-size 64 \
    --epochs 32 \
    --label-smoothing 1e-2 \
    --weight-decay 5e-2 \
    --no-obj-weight 1 \
-   ../dataset_defs/pre-training/yogo_parasite_data_with_tests.yml \
    --note "quarter_filters" \
    --lr-decay-factor 16 \
    --model quarter_filters \
@@ -15,13 +15,13 @@ sbatch scripts/submit_cmd_multi_gpu_light.sh yogo train \
    --normalize-images
 
 sbatch scripts/submit_cmd_multi_gpu_light.sh yogo train \
+   ../dataset_defs/pre-training/yogo_parasite_data_with_tests.yml \
    --lr 5e-4 \
    --batch-size 64 \
    --epochs 32 \
    --label-smoothing 1e-2 \
    --weight-decay 5e-2 \
    --no-obj-weight 1 \
-   ../dataset_defs/pre-training/yogo_parasite_data_with_tests.yml \
    --note "half_filters" \
    --lr-decay-factor 16 \
    --model half_filters \
@@ -29,13 +29,13 @@ sbatch scripts/submit_cmd_multi_gpu_light.sh yogo train \
    --normalize-images
 
 sbatch scripts/submit_cmd_multi_gpu_light.sh yogo train \
+   ../dataset_defs/pre-training/yogo_parasite_data_with_tests.yml \
    --lr 5e-4 \
    --batch-size 64 \
    --epochs 32 \
    --label-smoothing 1e-2 \
    --weight-decay 5e-2 \
    --no-obj-weight 1 \
-   ../dataset_defs/pre-training/yogo_parasite_data_with_tests.yml \
    --note "base" \
    --lr-decay-factor 16 \
    --model base_model \
@@ -43,13 +43,13 @@ sbatch scripts/submit_cmd_multi_gpu_light.sh yogo train \
    --normalize-images
 
 sbatch scripts/submit_cmd_multi_gpu_light.sh yogo train \
+   ../dataset_defs/pre-training/yogo_parasite_data_with_tests.yml \
    --lr 5e-4 \
    --batch-size 64 \
    --epochs 32 \
    --label-smoothing 1e-2 \
    --weight-decay 5e-2 \
    --no-obj-weight 1 \
-   ../dataset_defs/pre-training/yogo_parasite_data_with_tests.yml \
    --note "double_filters" \
    --lr-decay-factor 16 \
    --model double_filters \
@@ -57,13 +57,13 @@ sbatch scripts/submit_cmd_multi_gpu_light.sh yogo train \
    --normalize-images
 
 sbatch scripts/submit_cmd_multi_gpu_light.sh yogo train \
+   ../dataset_defs/pre-training/yogo_parasite_data_with_tests.yml \
    --lr 5e-4 \
    --batch-size 64 \
    --epochs 32 \
    --label-smoothing 1e-2 \
    --weight-decay 5e-2 \
    --no-obj-weight 1 \
-   ../dataset_defs/pre-training/yogo_parasite_data_with_tests.yml \
    --note "triple_filters" \
    --lr-decay-factor 16 \
    --model triple_filters \
