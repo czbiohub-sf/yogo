17009bc8cfcba500ef9eb2e3ab98857a4b7e669a
Axel Jacobsen <axelnj44@gmail.com>
2024-03-04 18:11:05 -0800

    upd submit finetune

--- scripts/submit_finetune.sh
+++ scripts/submit_finetune.sh
@@ -2,75 +2,75 @@
 
 sbatch scripts/submit_cmd_multi_gpu.sh yogo train \
    ../dataset_defs/human-labels/all-dataset-subsets-no-aug.yml \
-   --from-pretrained trained_models/rural-dragon-580/best.pth \
+   --from-pretrained trained_models/vibrant-rooster-557/best.pth \
    --lr 5e-5 \
    --batch-size 64 \
    --epochs 128 \
    --label-smoothing 5e-3 \
    --weight-decay 5e-3 \
    --no-obj-weight 1 \
-   --note "depth_ver_0" \
+   --note "quarter_filters" \
    --lr-decay-factor 16 \
-   --model depth_ver_0 \
-   --tags scaling-law depthwise fine-tune \
+   --model quarter_filters \
+   --tags scaling-law no-aug \
    --normalize-images
 
 sbatch scripts/submit_cmd_multi_gpu.sh yogo train \
    ../dataset_defs/human-labels/all-dataset-subsets-no-aug.yml \
-   --from-pretrained trained_models/sweet-tree-579/best.pth \
+   --from-pretrained trained_models/vermilion-noodles-558/best.pth \
    --lr 5e-5 \
    --batch-size 64 \
    --epochs 128 \
    --label-smoothing 5e-3 \
    --weight-decay 5e-3 \
    --no-obj-weight 1 \
-   --note "depth_ver_1" \
+   --note "half_filters" \
    --lr-decay-factor 16 \
-   --model depth_ver_1 \
-   --tags scaling-law depthwise fine-tune \
+   --model half_filters \
+   --tags scaling-law no-aug \
    --normalize-images
 
 sbatch scripts/submit_cmd_multi_gpu.sh yogo train \
    ../dataset_defs/human-labels/all-dataset-subsets-no-aug.yml \
-   --from-pretrained trained_models/woven-night-577/best.pth \
+   --from-pretrained trained_models/fortuitous-orchid-553/best.pth \
    --lr 5e-5 \
    --batch-size 64 \
    --epochs 128 \
    --label-smoothing 5e-3 \
    --weight-decay 5e-3 \
    --no-obj-weight 1 \
-   --note "depth_ver_2" \
+   --note "base" \
    --lr-decay-factor 16 \
-   --model depth_ver_2 \
-   --tags scaling-law depthwise fine-tune \
+   --model base_model \
+   --tags scaling-law no-aug \
    --normalize-images
 
-sbatch --dependency=afterok:12365763 scripts/submit_cmd_multi_gpu.sh yogo train \
+sbatch scripts/submit_cmd_multi_gpu.sh yogo train \
    ../dataset_defs/human-labels/all-dataset-subsets-no-aug.yml \
-   --from-pretrained trained_models/helpful-frog-583/best.pth \
+   --from-pretrained trained_models/sparkling-paper-555/best.pth \
    --lr 5e-5 \
    --batch-size 64 \
    --epochs 128 \
    --label-smoothing 5e-3 \
    --weight-decay 5e-3 \
    --no-obj-weight 1 \
-   --note "depth_ver_3" \
+   --note "double_filters" \
    --lr-decay-factor 16 \
-   --model depth_ver_3 \
-   --tags scaling-law depthwise fine-tune \
+   --model double_filters \
+   --tags scaling-law no-aug \
    --normalize-images
 
-sbatch --dependency=afterok:12365764 scripts/submit_cmd_multi_gpu.sh yogo train \
+sbatch scripts/submit_cmd_multi_gpu.sh yogo train \
    ../dataset_defs/human-labels/all-dataset-subsets-no-aug.yml \
-   --from-pretrained trained_models/expert-cloud-582/best.pth \
+   --from-pretrained trained_models/crimson-mandu-556/best.pth \
    --lr 5e-5 \
    --batch-size 32 \
    --epochs 128 \
    --label-smoothing 5e-3 \
    --weight-decay 5e-3 \
    --no-obj-weight 1 \
-   --note "depth_ver_4" \
+   --note "triple_filters" \
    --lr-decay-factor 16 \
-   --model depth_ver_4 \
-   --tags scaling-law depthwise fine-tune \
+   --model triple_filters \
+   --tags scaling-law no-aug \
    --normalize-images
