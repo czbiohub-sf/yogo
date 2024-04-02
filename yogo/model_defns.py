b21ca91a77f0a05c83ab98cf494e86e407cead17
Axel Jacobsen <axelnj44@gmail.com>
2024-04-01 20:27:58 -0700

    add a SILU model

--- yogo/model_defns.py
+++ yogo/model_defns.py
@@ -77,6 +77,55 @@ def base_model(num_classes: int) -> nn.Module:
 
 
 @register_model
+def silu_model(num_classes: int) -> nn.Module:
+    conv_block_1 = nn.Sequential(
+        nn.Conv2d(1, 16, 3, stride=2, padding=1, bias=False),
+        nn.BatchNorm2d(16),
+        nn.SiLU(inplace=True),
+    )
+    conv_block_2 = nn.Sequential(
+        nn.Conv2d(16, 32, 3, padding=1),
+        nn.SiLU(inplace=True),
+        nn.Dropout2d(p=0.05),
+    )
+    conv_block_3 = nn.Sequential(
+        nn.Conv2d(32, 64, 3, stride=2, padding=1),
+        nn.SiLU(inplace=True),
+        nn.Dropout2d(p=0.1),
+    )
+    conv_block_4 = nn.Sequential(
+        nn.Conv2d(64, 128, 3, padding=1),
+        nn.SiLU(inplace=True),
+        nn.Dropout2d(p=0.15),
+    )
+    conv_block_5 = nn.Sequential(
+        nn.Conv2d(128, 128, 3, stride=2, padding=1, bias=False),
+        nn.BatchNorm2d(128),
+        nn.SiLU(inplace=True),
+    )
+    conv_block_6 = nn.Sequential(
+        nn.Conv2d(128, 128, 3, padding=1, bias=True),
+        nn.BatchNorm2d(128),
+        nn.SiLU(inplace=True),
+    )
+    conv_block_7 = nn.Sequential(
+        nn.Conv2d(128, 128, 3, padding=1),
+        nn.SiLU(inplace=True),
+    )
+    conv_block_8 = nn.Conv2d(128, 5 + num_classes, 1)
+    return nn.Sequential(
+        conv_block_1,
+        conv_block_2,
+        conv_block_3,
+        conv_block_4,
+        conv_block_5,
+        conv_block_6,
+        conv_block_7,
+        conv_block_8,
+    )
+
+
+@register_model
 def double_filters(num_classes: int) -> nn.Module:
     conv_block_1 = nn.Sequential(
         nn.Conv2d(1, 32, 3, stride=2, padding=1, bias=False),
