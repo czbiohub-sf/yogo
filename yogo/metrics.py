63341763bf11348b93c0bfd6c30bebc0f938920c
Axel Jacobsen <axelnj44@gmail.com>
2024-06-19 09:40:37 -0700

    add thresholds number of 500

--- yogo/metrics.py
+++ yogo/metrics.py
@@ -74,6 +74,7 @@ class Metrics:
                     num_classes=self.num_classes,
                     average=None,
                     validate_args=False,
+                    thresholds=500,
                     sync_on_compute=sync_on_compute,
                 ),
                 # per-class
